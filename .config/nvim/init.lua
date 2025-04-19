f6725d2d6ed922aa5ee953e2e3f40b2ded1b56ce
Quentin Richert <quentin@richert.co>
2025-04-19 21:40:51 +0200

    config(nvim): Set up Treesitter

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -42,11 +42,16 @@ local function smart_format()
     or filetype == "typescript"
     or filetype == "html"
     or filetype == "css"
-    or filetype == "markdown"
     or filetype == "yaml"
   then
     label = "prettier"
     cmds = { { "prettier", "--write", "--prose-wrap=always", "--print-width=72", filepath } }
+  elseif filetype == "markdown" then
+    label = "prettier + normalize-punctuation"
+    cmds = {
+      { "prettier", "--write", "--prose-wrap=always", "--print-width=72", filepath },
+      { "normalize-punctuation", filepath },
+    }
   elseif filetype == "lua" then
     label = "stylua"
     cmds = { { "stylua", "--indent-type=spaces", "--indent-width=2", filepath } }
@@ -247,8 +252,6 @@ require("lazy").setup({
 
         -- Make inlays less intrusive.
         vim.api.nvim_set_hl(0, "LspInlayHint", { fg = "Gray" })
-        -- For some reason, exception statements were mapped to error colors.
-        vim.api.nvim_set_hl(0, "pythonException", { link = "Statement" })
       end,
     },
 
@@ -259,6 +262,9 @@ require("lazy").setup({
         require("nvim-rooter").setup()
       end,
     },
+
+    -- TODO: Try: oil.nvim (edit your filesystem like a buffer)
+    -- https://www.youtube.com/watch?v=-r1mMg-yVZE
 
     -- File tree.
     {
@@ -296,6 +302,99 @@ require("lazy").setup({
 
     -- Wakatime time tracking (`:WakaTime[Today]`).
     { "wakatime/vim-wakatime", lazy = false },
+
+    --- Treesitter ---
+
+    {
+      "nvim-treesitter/nvim-treesitter",
+      build = ":TSUpdate",
+      config = function()
+        require("nvim-treesitter.configs").setup({
+          -- A list of parser names, or "all" (the listed parsers MUST always be installed)
+          ensure_installed = {
+            "bash",
+            "c",
+            "caddy",
+            "cpp",
+            "html",
+            "css",
+            "csv",
+            "dockerfile",
+            "editorconfig",
+            "fish",
+            "git_config",
+            "git_rebase",
+            "gitattributes",
+            "gitcommit",
+            "gitignore",
+            "javadoc",
+            "javascript",
+            "typescript",
+            "json",
+            -- "latex", -- Requires Treesitter CLI.
+            "lua",
+            "luadoc",
+            "make",
+            "markdown",
+            "markdown_inline",
+            "mermaid",
+            "nginx",
+            "po",
+            "python",
+            "query",
+            "regex",
+            "rust",
+            "sql",
+            "ssh_config",
+            "toml",
+            "vim",
+            "vimdoc",
+            "xml",
+            "yaml",
+          },
+
+          -- Install parsers synchronously (only applied to `ensure_installed`)
+          sync_install = false,
+
+          -- Automatically install missing parsers when entering buffer
+          -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+          auto_install = false,
+
+          -- List of parsers to ignore installing (or "all")
+          -- ignore_install = { "javascript" },
+
+          -- If you need to change the installation directory of the parsers (see -> Advanced Setup)
+          -- Default: ~/.local/share/nvim/site/parser/
+          -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
+
+          highlight = {
+            enable = true,
+
+            -- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
+            -- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
+            -- the name of the parser)
+            -- list of language that will be disabled
+
+            -- disable = { "c", "rust" },
+
+            -- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
+            disable = function(lang, buf)
+              local max_filesize = 100 * 1024 -- 100 KB
+              local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+              if ok and stats and stats.size > max_filesize then
+                return true
+              end
+            end,
+
+            -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+            -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+            -- Using this option may slow down your editor, and you may see some duplicate highlights.
+            -- Instead of true it can also be a list of languages
+            additional_vim_regex_highlighting = false,
+          },
+        })
+      end,
+    },
 
     --- LSP ---
 
@@ -536,3 +635,8 @@ require("lazy").setup({
     frequency = 3600 * 24 * 7,
   },
 })
+
+-- Disable all LSP semantic highlights (in favor of Treesitter).
+for _, group in ipairs(vim.fn.getcompletion("@lsp", "highlight")) do
+  vim.api.nvim_set_hl(0, group, {})
+end
