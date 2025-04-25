1ababf8f6b2918e2e29e813105931b63283034ab
Quentin Richert <quentin@richert.co>
2025-04-25 10:50:33 +0200

    config(ghostty): Set SSH terminfo on `sync`

--- post-sync.sh
+++ post-sync.sh
@@ -13,3 +13,10 @@ if [[ -f ~/.deezfish.fish ]]; then
     echo -e "\n# <deez>\n" >> ~/.config/fish/config.fish
     cat ~/.deezfish.fish >> ~/.config/fish/config.fish
 fi
+
+# Alias SSH terminfo for Ghostty.
+if ! grep -qF "SetEnv TERM=xterm-256color" ~/.ssh/config; then
+    [[ -n $DEEZ_VERBOSE ]] && echo "Alias SSH terminfo for Ghostty."
+    echo "" >> ~/.ssh/config
+    cat ./.config/ghostty/ssh.txt >> ~/.ssh/config
+fi
