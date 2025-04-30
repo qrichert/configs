a6a5745c9905e5bc16e73df72d4936daeba1dbd8
Quentin Richert <quentin@richert.co>
2025-04-30 23:12:05 +0200

    config(nvim): Add auto-session plugin

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -313,6 +313,19 @@ require("lazy").setup({
     },
 
     {
+      "rmagatti/auto-session",
+      lazy = false,
+
+      ---enables autocomplete for opts
+      ---@module "auto-session"
+      ---@type AutoSession.Config
+      opts = {
+        -- suppressed_dirs = { "~/", "~/Projects", "~/Downloads", "/" },
+        -- log_level = 'debug',
+      },
+    },
+
+    {
       "shortcuts/no-neck-pain.nvim",
       version = "*",
       config = function()
