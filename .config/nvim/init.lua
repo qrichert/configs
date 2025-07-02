e4d5f4b3dbce52116171f1bb6a185129619bbecd
Quentin Richert <quentin@richert.co>
2025-07-02 19:13:06 +0200

    config(nvim): Set up Lua LSP

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -531,7 +531,7 @@ require("lazy").setup({
       "mason-org/mason-lspconfig.nvim",
       opts = {
         -- For configuration, see `vim.lsp.enable("...")`.
-        ensure_installed = { "jedi_language_server", "ruff", "rust_analyzer" },
+        ensure_installed = { "jedi_language_server", "lua_ls", "ruff", "rust_analyzer" },
       },
       dependencies = {
         { "mason-org/mason.nvim", opts = {} },
@@ -644,6 +644,57 @@ vim.lsp.enable("rust_analyzer")
 -- Python.
 vim.lsp.enable("ruff")
 vim.lsp.enable("jedi_language_server")
+
+-- Lua + Neovim API integration.
+vim.lsp.enable("lua_ls")
+vim.lsp.config("lua_ls", {
+  on_init = function(client)
+    if client.workspace_folders then
+      local path = client.workspace_folders[1].name
+      if
+        path ~= vim.fn.stdpath("config")
+        and (vim.uv.fs_stat(path .. "/.luarc.json") or vim.uv.fs_stat(path .. "/.luarc.jsonc"))
+      then
+        return
+      end
+    end
+
+    client.config.settings.Lua = vim.tbl_deep_extend("force", client.config.settings.Lua, {
+      runtime = {
+        -- Tell the language server which version of Lua you're using (most
+        -- likely LuaJIT in the case of Neovim)
+        version = "LuaJIT",
+        -- Tell the language server how to find Lua modules same way as Neovim
+        -- (see `:h lua-module-load`)
+        path = {
+          "lua/?.lua",
+          "lua/?/init.lua",
+        },
+      },
+      -- Make the server aware of Neovim runtime files
+      workspace = {
+        checkThirdParty = false,
+        library = {
+          vim.env.VIMRUNTIME,
+          -- Depending on the usage, you might want to add additional paths
+          -- here.
+          -- '${3rd}/luv/library'
+          -- '${3rd}/busted/library'
+        },
+        -- Or pull in all of 'runtimepath'.
+        -- Note: this is a lot slower and will cause issues when working on
+        -- your own configuration.
+        -- See https://github.com/neovim/nvim-lspconfig/issues/3189
+        -- library = {
+        --   vim.api.nvim_get_runtime_file('', true),
+        -- }
+      },
+    })
+  end,
+  settings = {
+    Lua = {},
+  },
+})
 
 vim.api.nvim_create_autocmd("LspAttach", {
   group = vim.api.nvim_create_augroup("UserLspConfig", {}),
