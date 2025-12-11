ecfbd0d77c665ed5a43c15de7ce0f81b44fa5d84
Quentin Richert <quentin@richert.co>
2025-12-11 12:35:26 +0100

    config(fish): Fix `history` command

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -49,7 +49,7 @@ function localip
 end
 
 function history
-    builtin history --show-time='%h/%d - %H:%M:%S ' | tail -r
+    builtin history --show-time='%h/%d - %H:%M:%S '
 end
 
 fish_add_path "$HOME/.local/bin"
