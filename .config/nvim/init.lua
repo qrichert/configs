57e8aec074a42a182937067b6c382cc13468bf71
Quentin Richert <quentin@richert.co>
2026-01-27 19:29:35 +0100

    config(nvim): Update `ggandor/leap.nvim` repo URL

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -203,7 +203,7 @@ require("lazy").setup({
 
     -- Quick navigation.
     {
-      "ggandor/leap.nvim",
+      url = "https://codeberg.org/andyg/leap.nvim",
       event = "VeryLazy",
       config = function()
         vim.keymap.set({ "n", "x", "o" }, "s", "<Plug>(leap)")
